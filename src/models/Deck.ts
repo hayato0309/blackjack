997f26af6f46212d1cec508e2e5c5e0626e52ca7
hayato0309 <hayato.pluto.4538@gmail.com>
2022-11-12 17:58:34 +0900

    create Deck model

---
+++ src/models/Deck.ts
@@ -0,0 +1,46 @@
+class Deck {
+    gameType: string;
+    cards: string[];
+
+    constructor(gameType: string) {
+        this.gameType = gameType;
+        this.cards = [];
+
+        this.createCards(gameType);
+    }
+
+    // ゲームの種類に応じてカードを作成する
+    createCards(gameType: string) {
+        if (gameType === "blackjack") {
+            const suit = ["H", "D", "C", "S"];
+            const rank = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+            for (let s = 0; s < suit.length; s++) {
+                for (let r = 0; r < rank.length; r++) {
+                    this.cards.push(suit[s] + rank[r]);
+                }
+            }
+        }
+    }
+
+    // カードをシャッフルする（Fisher-Yetesアルゴリズム）
+    shuffle() {
+        const n = this.cards.length;
+        for (let i = n - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const tmp = this.cards[i];
+            this.cards[i] = this.cards[j];
+            this.cards[j] = tmp;
+        }
+
+        console.log(this.cards);
+    }
+
+    resetDeck(gameType: string) {
+        this.createCards(gameType);
+    }
+
+    drawOne() {
+        return this.cards.shift();
+    }
+}
