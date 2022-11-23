2dccbd34d4acbad8ace23f8c039ce45acbd6b8d9
hayato0309 <hayato.pluto.4538@gmail.com>
2022-11-23 14:52:05 +0900

    evaluate player hand and update chips

--- src/models/Player.ts
+++ src/models/Player.ts
@@ -19,6 +19,20 @@ export class Player {
         this.gameDecision = <GameDecision>{};
     }
 
+    // getter
+    public getChips(): number {
+        return this.chips;
+    }
+
+    // setter
+    public setChips(chips: number): void {
+        this.chips = chips;
+    }
+
+    public setPlayerStatus(playerStatus: string): void {
+        this.playerStatus = playerStatus;
+    }
+
     // userの入力値に従ってgameDecisionを返す
     userPlayerGameDecision(actionParam: string, amountParam: number): void {
         const action: string = actionParam;
@@ -29,8 +43,23 @@ export class Player {
 
     // AIプレイヤーのgameDecisionを返す
     aiPlayerGameDecision(upCardRank: string, turnCounter: number): void {
-        const action: string = this.aiPlayerNextAction(upCardRank);
-        const amount: number = this.aiPlayerDecideBetAmount(turnCounter);
+        let action: string;
+        let amount: number;
+
+        action = this.aiPlayerNextAction(upCardRank);
+
+        // 2周目以降（turnCouner > 4）の場合はすでに設定している
+        if (turnCounter > 4) {
+            amount = this.gameDecision.amount;
+        } else {
+            if (action === "double") {
+                amount = this.aiPlayerDecideBetAmount() * 2;
+            } else if (action === "surrender") {
+                amount = this.aiPlayerDecideBetAmount() / 2;
+            } else {
+                amount = this.aiPlayerDecideBetAmount();
+            }
+        }
 
         this.gameDecision = new GameDecision(action, amount);
     }
@@ -43,31 +72,37 @@ export class Player {
         const upCardScore: number | string = this.rankToScore(upCardRank); // houseの一枚だけ表向きのカードのスコア（Aの場合はAをstringとして持つ。A以外の場合はnumber）
 
         if (handLength === 2) {
-            if (upCardScore === "A") {
-                // stand
-                if (handScore === 17) nextAction = "stand";
-                // surrender
-                if (handScore === 16) nextAction = "surrender";
-
-                // もしnextActionが空ストリングだったら、"hit"をアサイン
-                if (nextAction === "") nextAction = "hit";
+            if (handScore === 21) {
+                nextAction = "blackjack";
             } else {
-                // stand
-                if (handScore >= 17) nextAction = "stand";
-                if (handScore >= 13 && handScore <= 16 && upCardScore <= 6) nextAction = "stand";
-                if (handScore === 12 && upCardScore >= 3 && upCardScore <= 6) nextAction = "stand";
-                // surrender
-                if (handScore === 16 && upCardScore === 10) nextAction = "surrender";
-                if (handScore === 15 && upCardScore >= 9) nextAction = "surrender";
-                // double
-                if (handScore === 11) nextAction = "double";
-                if (handScore === 10 && upCardScore >= 9) nextAction = "double";
-                if (handScore === 9 && upCardScore >= 3 && upCardScore <= 6) nextAction = "double";
+                if (upCardScore === "A") {
+                    // stand
+                    if (handScore === 17) nextAction = "stand";
+                    // surrender
+                    if (handScore === 16) nextAction = "surrender";
 
-                // もしnextActionが空ストリングだったら、"hit"をアサイン
-                if (nextAction === "") nextAction = "hit";
+                    // もしnextActionが空ストリングだったら、"hit"をアサイン
+                    if (nextAction === "") nextAction = "hit";
+                } else {
+                    // stand
+                    if (handScore >= 17) nextAction = "stand";
+                    if (handScore >= 13 && handScore <= 16 && upCardScore <= 6) nextAction = "stand";
+                    if (handScore === 12 && upCardScore >= 3 && upCardScore <= 6) nextAction = "stand";
+                    // surrender
+                    if (handScore === 16 && upCardScore === 10) nextAction = "surrender";
+                    if (handScore === 15 && upCardScore >= 9) nextAction = "surrender";
+                    // double
+                    if (handScore === 11) nextAction = "double";
+                    if (handScore === 10 && upCardScore >= 9) nextAction = "double";
+                    if (handScore === 9 && upCardScore >= 3 && upCardScore <= 6) nextAction = "double";
+
+                    // もしnextActionが空ストリングだったら、"hit"をアサイン
+                    if (nextAction === "") nextAction = "hit";
+                }
             }
-        } else if (handLength >= 3) {
+        }
+
+        if (handLength >= 3) {
             if (handScore < 17) nextAction = "hit";
             else nextAction = "stand";
         }
@@ -90,9 +125,8 @@ export class Player {
     }
 
     // AIプレイヤーの掛け金を決める（5の倍数）
-    aiPlayerDecideBetAmount(turnCounter: number): number {
+    aiPlayerDecideBetAmount(): number {
         if (this.playerStatus === "broke") return 0;
-        if (turnCounter > 4) return 0; // 2周目に入っている場合は掛け金は0にしておく
 
         let betAmount: number = 0;
         let budgetForOneRound = this.chips / 5; // 1ラウンドの掛け金上限
@@ -122,13 +156,14 @@ export class Player {
         let sum = 0;
         let aCounter = 0; // 手札にあるAの枚数をカウント
         for (let i = 0; i < hand.length; i++) {
-            if (hand[i][1] == "A") {
+            const rank = hand[i].slice(1);
+            if (rank === "A") {
                 sum += 11;
                 aCounter++;
                 continue;
             };
-            if (face.includes(hand[i][1])) sum += 10;
-            else sum += Number(hand[i][1]);
+            if (face.includes(rank)) sum += 10;
+            else sum += Number(rank);
         }
 
         // 合計が21を超える場合、21以下になるまでAの枚数を上限として10を引き続ける
@@ -137,6 +172,7 @@ export class Player {
             aCounter -= 1;
         }
 
+        console.log(sum);
         return sum;
     }
 }
--- src/models/Table.ts
+++ src/models/Table.ts
@@ -70,37 +70,111 @@ export class Table {
     // gameDecisionに応じてプレイヤーの手札・playerStatus・チップを更新する
     // 全てのプレイヤーのplayerStatusがroundOverになるまで繰り返し実行される
     executeGameDecision(player: Player): void {
-        if (player.gameDecision.action === "stand") {
+        const paidBet: boolean = this.turnCounter > 4; // すでにbetを支払っているか（turnCounterが4より大きい場合はすでに支払っている）
+        const amount = player.gameDecision.amount;
+
+        if (player.gameDecision.action === "blackjack") {
+            this.updatePlayerChips(player, -amount);
+            player.setPlayerStatus("readyForActing");
+
+        } else if (player.gameDecision.action === "stand") {
             // 掛け金をchipsから引き、ラウンドを終了する
-            player.chips -= player.gameDecision.amount;
-            player.playerStatus = "readyForActing";
+            if (!paidBet) this.updatePlayerChips(player, -amount);
+            player.setPlayerStatus("readyForActing");
+
         } else if (player.gameDecision.action === "hit") {
             // 掛け金をchipsから引き、もう一枚カードを引く
-            player.chips -= player.gameDecision.amount;
+            if (!paidBet) this.updatePlayerChips(player, -amount);
             player.hand.push(this.deck.drawOne());
+
         } else if (player.gameDecision.action === "double") {
-            // 掛け金の2倍をchipsから引き、もう一枚カードを引く
-            player.chips -= (player.gameDecision.amount * 2);
+            // 掛け金の2倍をchipsから引き、もう一枚カードを引く。ダブルを宣言すると1枚しかカードを追加できない
+            this.updatePlayerChips(player, -amount);
             player.hand.push(this.deck.drawOne());
+            player.setPlayerStatus("readyForActing");
+
         } else if (player.gameDecision.action === "surrender") {
             // 掛け金の半分をchipsから引き、ラウンドを終了する
-            player.chips -= (player.gameDecision.amount / 2);
-            player.playerStatus = "readyForActing";
+            this.updatePlayerChips(player, -Math.ceil(amount / 2));
+            player.setPlayerStatus("readyForActing");
         }
     }
 
     // プレイヤーがbustか判定
     checkIfPlayerIsBust(player: Player): void {
-        if (player.getHandScore() > 21) player.playerStatus = "bust";
+        if (player.getHandScore() > 21) player.setPlayerStatus("bust");
     }
 
     // 全てのプレイヤーがbettingを終了しているか判定
-    checkIfReadyForActing(): void {
+    checkIfAllPlayersReadyForActing(): void {
         let doneBettingCounter = 0;
         this.players.map((player) => {
             if (player.playerStatus !== "betting") doneBettingCounter++;
         });
 
         if (doneBettingCounter === 4) this.gamePhase = "acting";
+    }
+
+    // 勝敗判定
+    getWinner(house: Player, player: Player): string {
+        let winner: string = ""; // houseかplayer
+
+        if (player.playerStatus === "surrender") winner = "house";
+
+        if (house.playerStatus === "bust" || player.playerStatus === "bust") {
+            // 少なくともhouseかplayerのどちらかがbustの場合
+            if (house.playerStatus === "bust") {
+                winner = "player";
+            } else if (player.playerStatus === "bust") {
+                winner = "house";
+            } else if (house.playerStatus === "bust" && player.playerStatus === "bust") {
+                winner = "house"; // houseとplayerの両方がbustの時はhouseが勝利する
+            }
+        } else {
+            // houseもplayerもbustしてない場合
+            const houseScore: number = house.getHandScore();
+            const playerScore: number = player.getHandScore();
+
+            if (houseScore === playerScore ||
+                (house.gameDecision.action === "blackjack" && player.gameDecision.action === "blackjack")) winner = "no one"; // 引き分け
+            else winner = houseScore > playerScore ? "house" : "player";
+        }
+
+        return winner;
+    }
+
+    // 勝敗判定に応じてchipsの増減額を計算（勝てば正の数、負ければ負の数を返す）
+    calcDevidend(house: Player, player: Player) {
+        let houseDevidend: number = 0;
+        let playerDevidend: number = 0;
+
+        const winner = this.getWinner(house, player);
+
+        if (winner === "no one") {
+            // do nothing
+        } else if (winner === "house") {
+            if (house.gameDecision.action === "blackjack") {
+                houseDevidend = house.gameDecision.amount * 2.5;
+            } else {
+                houseDevidend = house.gameDecision.amount * 2;
+            }
+        } else if (winner === "player") {
+            if (player.gameDecision.action === "blackjack") {
+                playerDevidend = player.gameDecision.amount * 2.5; // blackjackの場合の配当は2.5倍
+            } else if (player.gameDecision.action === "double") {
+                playerDevidend = player.gameDecision.amount * 2 * 2; // ダブルを宣言して勝った場合の配当は4倍
+            } else {
+                playerDevidend = player.gameDecision.amount * 2;
+            }
+        }
+
+        return { house: houseDevidend, player: playerDevidend };
+    }
+
+    // 金額を受け取りプレイヤーのchipsを更新する
+    updatePlayerChips(player: Player, amount: number): void {
+        const currChips = player.getChips();
+        const newChips = currChips + amount;
+        player.setChips(newChips);
     }
 }
