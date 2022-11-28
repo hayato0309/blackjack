71e9d81f4216ab19e8115934e17b5fb5a7fc7f51
hayato0309 <hayato.pluto.4538@gmail.com>
2022-11-29 06:52:36 +0900

    created start game view

--- src/controllers/blackjack.ts
+++ src/controllers/blackjack.ts
@@ -2,10 +2,8 @@ import { View } from './../views/blackjack';
 
 export class Controller {
     static displayGameSelectPage() {
-        if (typeof document !== "undefined") {
-            const containerDom = document.getElementById("container")!;
-            console.log(containerDom);
-            View.createGameSelectPage(containerDom);
-        }
+        const containerDom = document.getElementById("container")!;
+        View.createGameSelectPage(containerDom);
+        console.log(containerDom);
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,44 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { Card } from './models/Card';
+import { Deck } from './models/Deck';
+import { Player } from './models/Player';
+import { GameDecision } from './models/GameDecision';
+import { Table } from './models/Table';
+import { Controller } from './controllers/blackjack';
 
-import confetti from 'canvas-confetti';
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const table = new Table("blackjack", [1, 5, 20, 50, 100]);
+
+table.blackjackAssignPlayerHands();
+
+const house = table.players[0];
+
+const houseUpCardRank = table.players[0].hand[0].slice(1);
+
+while (table.gamePhase === "betting") {
+  const currPlayer = table.getTurnPlayer();
+
+  if (currPlayer.playerStatus === "betting") {
+    table.promptNextAiActionAndBet(currPlayer, houseUpCardRank, table.turnCounter);
+    table.executeGameDecision(currPlayer);
+  }
+
+  table.checkIfPlayerIsBust(currPlayer);
+
+  table.checkIfAllPlayersReadyForActing();
+}
+
+table.players.map((player) => {
+  if (player.type !== "house") {
+    const devidends = table.calcDevidend(house, player);
+    table.updatePlayerChips(house, devidends.house);
+    table.updatePlayerChips(player, devidends.player);
+  }
+})
+
+table.players.map((player) => {
+  console.log(player);
+})
+
+table.blackjackClearPlayerHandsAndBets();
+
+Controller.displayGameSelectPage();
