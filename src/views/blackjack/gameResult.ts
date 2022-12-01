565866f4512ac201233cdafa8f25048bc3e0c58c
hayato0309 <hayato.pluto.4538@gmail.com>
2022-12-02 08:14:22 +0900

    adjusted game result modal design

--- src/views/blackjack/gameResult.ts
+++ src/views/blackjack/gameResult.ts
@@ -13,54 +13,54 @@ export class GameResultModal {
                     <table class="w-full h-3/4 mb-4">
                         <thead class="flex w-full rounded-t-xl bg-zinc-100">
                             <tr class="flex w-full border-b-2">
-                                <th class="py-3 w-2/12">ROUND</th>
-                                <th class="py-3 w-4/12">HAND</th>
-                                <th class="py-3 w-3/12">WIN / LOSE</th>
-                                <th class="py-3 w-3/12">CHIPS ⇅</th>
+                                <th class="py-4 w-2/12">ROUND</th>
+                                <th class="py-4 w-4/12">HAND</th>
+                                <th class="py-4 w-3/12">WIN / LOSE</th>
+                                <th class="py-4 w-3/12">CHIPS ⇅</th>
                             </tr>
                         </thead>
                         <tbody class="flex flex-col items-center justify-between overflow-y-scroll w-full h-5/6 shadow-xl shadow-inner rounded-b-xl">
                             <tr class="flex w-full border-b">
-                                <td class="text-center py-3 w-2/12">1</td>
-                                <td class="text-center py-3 w-4/12">HA, S10</td>
-                                <td class="text-center py-3 w-3/12">WIN</td>
-                                <td class="text-center py-3 w-3/12">+ $50</td>
+                                <td class="text-center py-4 w-2/12">1</td>
+                                <td class="text-center py-4 w-4/12">HA, S10</td>
+                                <td class="text-center py-4 w-3/12">WIN</td>
+                                <td class="text-center py-4 w-3/12">+ $50</td>
                             </tr>
                             <tr class="flex w-full border-b">
-                                <td class="text-center py-3 w-2/12">1</td>
-                                <td class="text-center py-3 w-4/12">HA, S10</td>
-                                <td class="text-center py-3 w-3/12">WIN</td>
-                                <td class="text-center py-3 w-3/12">+ $50</td>
+                                <td class="text-center py-4 w-2/12">1</td>
+                                <td class="text-center py-4 w-4/12">HA, S10</td>
+                                <td class="text-center py-4 w-3/12">WIN</td>
+                                <td class="text-center py-4 w-3/12">+ $50</td>
                             </tr>
                             <tr class="flex w-full border-b">
-                                <td class="text-center py-3 w-2/12">1</td>
-                                <td class="text-center py-3 w-4/12">HA, S10</td>
-                                <td class="text-center py-3 w-3/12">WIN</td>
-                                <td class="text-center py-3 w-3/12">+ $50</td>
+                                <td class="text-center py-4 w-2/12">1</td>
+                                <td class="text-center py-4 w-4/12">HA, S10</td>
+                                <td class="text-center py-4 w-3/12">WIN</td>
+                                <td class="text-center py-4 w-3/12">+ $50</td>
                             </tr>
                             <tr class="flex w-full border-b">
-                                <td class="text-center py-3 w-2/12">1</td>
-                                <td class="text-center py-3 w-4/12">HA, S10</td>
-                                <td class="text-center py-3 w-3/12">WIN</td>
-                                <td class="text-center py-3 w-3/12">+ $50</td>
+                                <td class="text-center py-4 w-2/12">1</td>
+                                <td class="text-center py-4 w-4/12">HA, S10</td>
+                                <td class="text-center py-4 w-3/12">WIN</td>
+                                <td class="text-center py-4 w-3/12">+ $50</td>
                             </tr>
                             <tr class="flex w-full border-b">
-                                <td class="text-center py-3 w-2/12">1</td>
-                                <td class="text-center py-3 w-4/12">HA, S10</td>
-                                <td class="text-center py-3 w-3/12">WIN</td>
-                                <td class="text-center py-3 w-3/12">+ $50</td>
+                                <td class="text-center py-4 w-2/12">1</td>
+                                <td class="text-center py-4 w-4/12">HA, S10</td>
+                                <td class="text-center py-4 w-3/12">WIN</td>
+                                <td class="text-center py-4 w-3/12">+ $50</td>
                             </tr>
                             <tr class="flex w-full border-b">
-                                <td class="text-center py-3 w-2/12">1</td>
-                                <td class="text-center py-3 w-4/12">HA, S10</td>
-                                <td class="text-center py-3 w-3/12">WIN</td>
-                                <td class="text-center py-3 w-3/12">+ $50</td>
+                                <td class="text-center py-4 w-2/12">1</td>
+                                <td class="text-center py-4 w-4/12">HA, S10</td>
+                                <td class="text-center py-4 w-3/12">WIN</td>
+                                <td class="text-center py-4 w-3/12">+ $50</td>
                             </tr>
                             <tr class="flex w-full border-b">
-                                <td class="text-center py-3 w-2/12">1</td>
-                                <td class="text-center py-3 w-4/12">HA, S10</td>
-                                <td class="text-center py-3 w-3/12">WIN</td>
-                                <td class="text-center py-3 w-3/12">+ $50</td>
+                                <td class="text-center py-4 w-2/12">1</td>
+                                <td class="text-center py-4 w-4/12">HA, S10</td>
+                                <td class="text-center py-4 w-3/12">WIN</td>
+                                <td class="text-center py-4 w-3/12">+ $50</td>
                             </tr>
                         </tbody>
                     </table>
