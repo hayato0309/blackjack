e04f00324250dca61b2047f56b1229d66f8c9873
hayato0309 <hayato.pluto.4538@gmail.com>
2023-01-07 18:25:17 +0900

    changed gameSetting page design

--- src/views/blackjack/pages/gameSetting.ts
+++ src/views/blackjack/pages/gameSetting.ts
@@ -6,14 +6,14 @@ export class GameSettingPage {
     static createGameSettingPage(table: Table): void {
         CONTAINER.innerHTML = `
             <div class="flex justify-center items-center h-screen w-screen">
-                <div class="w-6/12 rounded-lg bg-white shadow-lg flex">
-                    <div class="w-5/12 bg-sky-300 rounded-l-lg">image</div>
+                <div class="w-6/12 rounded-2xl bg-white shadow-lg flex">
+                    <img class="w-5/12 object-cover rounded-l-2xl" src="https://images.unsplash.com/photo-1511193311914-0346f16efe90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1773&q=80">
                     <div class="w-7/12 p-12">
                         <form id="gameSettingForm">
                             <div class="mb-4">
                                 <label class="block mb-2 font-bold text-gray-700">Name</label>
                                 <input
-                                    class="w-full px-3 py-2 border shadow rounded"
+                                    class="w-full px-3 py-2 border shadow rounded-xl focus:outline-teal-500"
                                     id="userName"
                                     type="text"
                                     placeholder="Name"
@@ -22,7 +22,7 @@ export class GameSettingPage {
                             </div>
                             <div class="mb-4">
                                 <label class="block mb-2 font-bold text-gray-700">Select Game</label>
-                                <select class="w-full px-3 py-2 border shadow rounded" id="gameType">
+                                <select class="w-full px-3 py-2 border shadow rounded-xl focus:outline-teal-500" id="gameType">
                                     <option value="blackjack">Blackjack</option>
                                     <option value="porker" disabled>Porker - coming soon -</option>
                                     <option value="baccarat" disabled>Baccarat - coming soon -</option>
@@ -30,14 +30,14 @@ export class GameSettingPage {
                             </div>
                             <div class="mb-8">
                                 <label class="block mb-2 font-bold text-gray-700">Computer Player Speed</label>
-                                <select class="w-full px-3 py-2 border shadow rounded" id="computerPlayerSpeed">
+                                <select class="w-full px-3 py-2 border shadow rounded-xl focus:outline-teal-500" id="computerPlayerSpeed">
                                     <option value="fast">Fast</option>
                                     <option value="middle">Middle</option>
                                     <option value="slow">Slow</option>
                                 </select>
                             </div>
                             <button
-                                class="w-full px-4 py-2 font-bold text-white bg-zinc-800 rounded-full hover:bg-zinc-600 focus:outline-none focus:shadow-outline"
+                                class="w-full px-4 py-2 font-bold text-white bg-zinc-800 rounded-xl hover:bg-teal-500 focus:outline-none focus:shadow-outline"
                                 type="submit"
                             >
                             GET STARTED
